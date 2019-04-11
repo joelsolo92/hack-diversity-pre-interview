2d12c7a8f8024bdcce464158d7e2df3b2959e37c
Eyoel Demissie <eyoelsolomon@gmail.com>
2019-04-11 16:20:19 -0400

    Update ConversationMetricsCalculator.java

--- src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
+++ src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
@@ -22,11 +22,16 @@ public class ConversationMetricsCalculator {
     for (int x = 0; x < messages.size() - 1; x++){
 
       currentMessage = messages.get(x);
+
       for (int y = x+1; y < messages.size(); y++){
+
         nextMessage = messages.get(y);
+
         if (currentMessage.isTeamMember() == false && nextMessage.isTeamMember() == true){
+
             totalTimeDifferenceMs += (nextMessage.getCreatedAt() - currentMessage.getCreatedAt());
             numOfResponses++;
+
             if (y < messages.size() - 1)
               x = y;
             break;
